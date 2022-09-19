e9803456cb7f3a07cc809477f163ba911ea7aec5
Fernando <fernando.rodrigues@compasso.com.br>
2022-09-19 16:14:32 -0300

    new project

--- forno/main.py
+++ forno/main.py
@@ -5,12 +5,11 @@ botao = input("ON/OFF: ").upper()
 
 if botao == "ON":
     humidadeInterna = float(input("Humidade interna do forno: "))
-    humidadeExterna = float(input("Humidade externa do forno: "))
     temperaturaInterna = int(input("Temperatura interna (ºC): "))
-    def aquecerForno(temperatura):
+    def aquecerForno(temperatura, temperaturaDesejada):
         if temperatura < 200:
             print("Iniciando aquecimento...")
-            while(temperatura < 380):
+            while(temperatura < temperaturaDesejada):
                 print(f'{temperatura}ºC...')
                 temperatura += 40
                 sleep(1)
@@ -26,8 +25,28 @@ if botao == "ON":
         print(f'A humidade atual é de {humidade}%')
 
 
-    aquecerForno(temperaturaInterna)
+    print("Iniciando processo de desumidificação...")
+    sleep(2)
+    if temperaturaInterna > 15 and humidadeInterna >= 40:
+        ligarExaustor(humidadeInterna)
+    elif temperaturaInterna < 15 and humidadeInterna >= 40:
+        aquecerForno(temperaturaInterna, 100)
+        ligarExaustor(humidadeInterna)
+    print("Processo concluído")
+    print("Iniciando processo de cocção...")
+    sleep(2)
+    humidadeInterna = float(input("Humidade interna do forno: "))
     ligarExaustor(humidadeInterna)
+
+    temperaturaInterna = int(input("Temperatura interna (ºC): "))
+    if temperaturaInterna < 200:
+        aquecerForno(temperaturaInterna, 380)
+
+    iniciar = input("Inserir conteúdo para cocção - quando contluir presisionar o botão pronto: ").upper()
+    if iniciar == "PRONTO":
+        sleep(10800)
+
+    print(f'A temperatura atual do forno é de {temperaturaInterna}ºC e a humidade interna é de {humidadeInterna}ºC.')
 
 
 
