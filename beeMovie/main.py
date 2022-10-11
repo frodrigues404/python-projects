e9214149124884e89faf3569b0a88e9def355d95
Fernando <fernando.rodrigues@compasso.com.br>
2022-10-10 21:33:54 -0300

    Commit

--- beeMovie/main.py
+++ beeMovie/main.py
@@ -5,8 +5,10 @@ from time import sleep
 sleep(2)
 
 palavras = open("beemovie.txt", 'r')
-#palavras.close()
 
 for word in palavras:
     pyautogui.typewrite(word)
     pyautogui.press("enter")
+
+
+palavras.close()
